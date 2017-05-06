d45fa34acf8581e3aa9a052087cf5a84301f454d
Yanik Kaypinger <yanik@addster.eu>
2017-05-06 13:49:01 +0200

    add remainShip in placeShips

--- include/placeShips.php
+++ include/placeShips.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
         <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
     </head>
     <body>
@@ -43,6 +43,45 @@
                 </table>
                 <button class="btn btn-primary btn-lg" id="checkShips">Prüfen</button> <button class="btn btn-primary btn-lg disabled">Absenden</button>
                 </div>
+            </div>
+
+            <div class="remainShips">
+              <div class="row remainShips-2">
+                <div class="col-sm-3">
+                  <div class="row">
+                    <div class="col-sm-2 remainShip"></div>
+                    <div class="col-sm-2 remainShip"></div>
+                    <div class="col-sm-6 countShips">3</div>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="row remainShips-3">
+                    <div class="col-sm-2 remainShip"></div>
+                    <div class="col-sm-2 remainShip"></div>
+                    <div class="col-sm-2 remainShip"></div>
+                    <div class="col-sm-4 countShips">2</div>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="row remainShips-4">
+                    <div class="col-sm-2 remainShip"></div>
+                    <div class="col-sm-2 remainShip"></div>
+                    <div class="col-sm-2 remainShip"></div>
+                    <div class="col-sm-2 remainShip"></div>
+                    <div class="col-sm-2 countShips">2</div>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="row remainShips-5">
+                    <div class="col-sm-2 remainShip"></div>
+                    <div class="col-sm-2 remainShip"></div>
+                    <div class="col-sm-2 remainShip"></div>
+                    <div class="col-sm-2 remainShip"></div>
+                    <div class="col-sm-2 remainShip"></div>
+                    <div class="col-sm-1 countShips">1</div>
+                  </div>
+                </div>
+              </div>
             </div>
         </div>
         <script>
