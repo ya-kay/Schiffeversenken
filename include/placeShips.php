c066bff61ad8a47bd9a08961b3ca81b4c5cc5c32
Alexander Lübeck <ndsvw@bitbucket.org>
2017-05-08 19:04:52 +0200

    bugfix: end of javascript-tag added

--- include/placeShips.php
+++ include/placeShips.php
@@ -131,6 +131,7 @@
             // });
 
             // Zusätlich später, wenn Schiff neben bereits vorhandenden Schiff sitzt, kurz rotaufleuchten und Schiff nicht setzten, ggf. Fehlermeldung ausgeben
+        });
         </script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
